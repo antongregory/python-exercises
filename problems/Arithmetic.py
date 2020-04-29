496e34e9ae4919d507c4eddcdf70ce869e23d322
Anton <antonrose.g@gmail.com>
2020-04-29 21:05:00 +0200

    Additional problems

---
+++ problems/Arithmetic.py
@@ -0,0 +1,22 @@
+# Read two integers from STDIN and print three lines where:
+#
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+# Next print division of two numbers
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+    c = int(input())
+    d = int(input())
+
+    print(c//d)
+    print(c/d)
